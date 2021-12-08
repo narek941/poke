44082cfff06a6d2941150ba208e4cc5ed44659a9
Narek Kolyan <nqolyan@gmail.com>
2021-12-08 11:22:36 +0400

    First commit

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 import axios from "axios";
 
 import Header from "../../components/Header";
@@ -8,58 +8,47 @@ import List from "../../components/List";
 import Pagination from "../../components/Pagination";
 import { setPokemons, setTotalCount } from "../../redux/pokemonActions";
 
-import './Home.css';
+import "./Home.css";
 
-  
-
-
-
-const Home = ({pokemons,totalCount,setPokemons,setTotalCount}) => {
-
-
+const Home = ({ pokemons, totalCount, setPokemons, setTotalCount }) => {
   useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon/")
       .then((res) => {
         setTotalCount(res.data.count);
         return res.data.results;
-       
       })
       .then((results) => {
-        console.log(results)
+        console.log(results);
         return Promise.all(results.map((res) => axios.get(res.url)));
       })
       .then((results) => {
         setPokemons(results.map((res) => res.data));
       });
   }, []);
-  
-console.log(pokemons)
+
+  console.log(pokemons);
   return (
     <>
       <Header />
-   
-      <Bar />
-      <List pokemons={pokemons}/>
-     
-      
-     
-      <Pagination totalCount={totalCount} />
 
+      <Bar />
+      <List pokemons={pokemons} />
+
+      <Pagination totalCount={totalCount} />
     </>
   );
 };
 
-const mapStateToProps = state =>{
+const mapStateToProps = (state) => {
   return {
     pokemons: state.pokemons.pokemons,
     totalCount: state.pokemons.totalCount,
-}
-}
+  };
+};
 const mapDispatchToProps = {
   setPokemons,
   setTotalCount,
-}
+};
 
-
-export default connect(mapStateToProps,mapDispatchToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
