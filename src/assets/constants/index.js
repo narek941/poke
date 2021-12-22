2e239bfa89514d2e07cb99c3cde5cd6d29f1f864
Narek Kolyan <nqolyan@gmail.com>
2021-12-22 18:01:09 +0400

    adding map for options

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -19,6 +19,7 @@ export const typeArr = [
   "fairy",
 ];
 export const perPageArr = [25, 50, 100];
+
 export const filterArr = [
   "Lowest to highest number",
   "Highest to lowest number",
@@ -28,7 +29,10 @@ export const filterArr = [
 
 export const statsWithColorStyle =
   "pokemonsDetail__stat__box__inner color_for_stat";
+
 export const statsWithoutColorStyle = "pokemonsDetail__stat__box__inner";
+
 export const baseUrl = "https://pokeapi.co/api/v2/";
+
 export const imgUrl =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/";
